86e77a0e9f2a55f074dbabdc2221c572c4d6fe32
kalistos1 <ucktech1@gmail.com>
2022-12-25 23:35:30 +0100

    remove static in livetos inner

--- livetos/settings.py
+++ livetos/settings.py
@@ -137,14 +137,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 #if DEBUG:
-STATICFILES_DIRS = [
-        os.path.join (BASE_DIR ,'livetos/static/')
-    ]
+#STATICFILES_DIRS = [
+#        os.path.join (BASE_DIR ,'livetos/static/')
+ #   ]
 #else:
-STATIC_ROOT = os.path.join(BASE_DIR,'static/')
+#STATIC_ROOT = os.path.join(BASE_DIR,'static/')
 
+STATICFILES_DIRS = [
+    BASE_DIR/ "static", "./static/",
+]
 
 STATIC_URL = '/static/'
+
+
 
 LOGIN_REDIRECT_URL = 'dashboard:profile'
 LOGIN_URL = 'auth:signin'
